a11c966d92c0848b6fa0c6428c4323f77d09a005
kams <kams1011@naver.com>
2023-08-08 01:35:57 +0900

    커밋

---
+++ test-code-with-architecture-main/test-code-with-architecture-main/src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,4 @@
+
+
+insert into users (id, email, nickname, address, certification_code, status, last_login_at)
+values(1, 'kams1011@naver.com', 'kams', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaa', 'ACTIVE', 0);
