3d601aadea6b3b868a5a65dbe6368c383c63bac7
kams <kams1011@naver.com>
2023-08-10 19:22:15 +0900

    UserController 테스트 추가

---
+++ test-code-with-architecture-main/test-code-with-architecture-main/src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,7 @@
+
+
+insert into users (id, email, nickname, address, certification_code, status, last_login_at)
+values(1, 'kams1011@naver.com', 'kams', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaa', 'ACTIVE', 0);
+
+insert into users (id, email, nickname, address, certification_code, status, last_login_at)
+values(2, 'kams1012@naver.com', 'kams2', 'Seoul2', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaab', 'PENDING', 0);
