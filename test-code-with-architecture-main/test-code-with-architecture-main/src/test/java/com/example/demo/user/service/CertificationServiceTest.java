4dbf00c1ba21692588bbc434aad96bc59a3b45d5
kams <79346265+kams1011@users.noreply.github.com>
2023-08-11 17:48:07 +0900

    CertificationService 테스트 추가

---
+++ test-code-with-architecture-main/test-code-with-architecture-main/src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,22 @@
+package com.example.demo.user.service;
+
+import com.example.demo.mock.FakeMailSender;
+import com.example.demo.user.service.port.MailSender;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CertificationServiceTest {
+
+    @Test
+    public void 이메일과_컨텐츠가_제대로_만들어져서_보내지는지_테스트한다(){
+        FakeMailSender fakeMailSender = new FakeMailSender();
+        //given
+        CertificationService certificationService = new CertificationService(fakeMailSender);
+        //when
+        certificationService.send("kams1011@naver.com", 1, "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaa");
+        //then
+        assertThat(fakeMailSender.email).isEqualTo("kams1011@naver.com");
+        assertThat(fakeMailSender.title).isEqualTo("Please certify your email address");
+    }
+}
